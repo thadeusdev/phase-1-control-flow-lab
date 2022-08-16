d610f07fcad284949f0c26f9b3e338e9a1c01cf6
thadeus <thadeusnyariki94@gmail.com>
2022-08-16 10:37:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+  if(value <= 400){
+    return 'This one is on me!';
+  }else if(value <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  }else{
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city === 'NYC'){
+    return 'Ok, sounds good.';
+  }else{
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';;
+    case 'not as generous':
+      return 'Thank you.'; 
+    default:
+      return 'Bye.'; 
+  }
 }
